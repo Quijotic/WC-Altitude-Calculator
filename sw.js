c4e177739260d18d45443281cfba1ddbca0f8f6d
Quijotic <doughbeebros@gmail.com>
2026-04-24 14:37:22 -0400

    Update sw.js AND MAYBE IT ALL WORKS NOW?

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 const CACHE_NAME = 'wc-calc-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
